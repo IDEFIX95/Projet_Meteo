73971eaca3b7943c3d33ef512fcc1d55c612a2eb
IDEFIX95 <104372704+IDEFIX95@users.noreply.github.com>
2023-01-16 09:34:21 +0100

    Update donneefiltre.bash

--- donneefiltre.bash
+++ donneefiltre.bash
@@ -5,8 +5,6 @@
     #exit 1
 #fi
 
-
-
 # les variables pour les options
 temperature=0
 pression=0
@@ -14,8 +12,10 @@ vent=0
 humidite=0
 altitude=0
 count=0
+
 fichierdentree=''
 option_oblig=0
+option_geo=0
 
 #les variables pour les numeros de champ du fichier d'entrée du csv
 numero_station=1
@@ -40,7 +40,7 @@ numero_commune=15
 #fi
 
 ##Gestion des parametres
-while getopts ":t:p:whmdf:-:" option
+while getopts ":whmf:d:t:p:-:" option;
 do
     # Commencons par la gestion des options longues
     if [ "$option" = '-' ]
@@ -68,6 +68,7 @@ do
                 echo "creation du fichier help"
                 touch dochelp.txt
                 chmod 744 dochelp.txt
+                option_oblig=$(($option_oblig+1))
             ;;
             sup)
                 sup=1
@@ -75,7 +76,7 @@ do
                 rm *.txt
                 echo "la suppression des csv vont etre executées sous demande"
                 rm -i *.csv
-                option_oblig+=1
+                option_oblig=$(($option_oblig+1))
             ;;
             *)
                 echo "option longue non permise --${OPTARG}"
@@ -83,7 +84,10 @@ do
             ;;
         esac
     else
-        case "$option" in       #Test des options courtes
+        case "$option" in           #Test des options courtes
+        f)
+            fichierdentree=${OPTARG}
+            ;;      
         t)
             temperature=${OPTARG}
             numero='^[1-3]+$'
@@ -91,6 +95,7 @@ do
             echo "Choix des numeros invalide. Veuillez reselectionner un numero de mode compris entre 1 et 3"
             exit 1
             fi
+            option_oblig=$(($option_oblig+1))
             ;;
         p)
             pression=${OPTARG}
@@ -98,23 +103,45 @@ do
             if ! [[ $pression =~ $numero ]];then
             echo "Choix du numero invalide. Veuillez reselectionner un numero de mode compris entre 1 et 3"
             exit 1
-            fi      
+            fi   
+            option_oblig=$(($option_oblig+1))
             ;;
         w)
-            vent=${OPTARG}
-            echo "bien joué"
+            vent=${option}
+            #echo "bien joué"
+            option_oblig=$(($option_oblig+1))
             ;;
         h)
-            altitude=${OPTARG}
-            echo "bien joué"
+            altitude=${option}
+            #echo "bien joué"
+            option_oblig=$(($option_oblig+1))
             ;;
         m)
-            humidite=${OPTARG}
-            echo "bien joué"
+            humidite=${option}
+            #echo "bien joué"
+            option_oblig=$(($option_oblig+1))
             ;;
-        f)
-            fichierdentree=${OPTARG}
+        F)
+            #awk -F";" 'NR {print '$numero_coordonne'}' meteo_filtered_data_v1.csv
+            option_geo=$(($option_geo+1))
             ;;
+        G)
+            option_geo=$(($option_geo+1))
+        ;;
+        S)
+            #awk -F ";" '{split('$numero_coordonne', coord, ";"); x = coord[1]; y = coord[2]; if (x < 48 && x > 46 && y > 55 && y < 57 ) {print x,y}}' meteo_filtered_data_v1.csv
+            option_geo=$(($option_geo+1))
+        ;;
+        A)
+            option_geo=$(($option_geo+1))
+        ;;
+        O)
+            option_geo=$(($option_geo+1))
+        ;;
+        Q)
+            option_geo=$(($option_geo+1))
+            echo "bonjour"
+        ;;
         :)
             echo "Erreur: -${OPTARG} argument recommander."
             exit 1
@@ -130,6 +157,7 @@ do
     fi
 done
 
+## Les nombreux testent à effectuer pour l'instant programmer comme cela
 
 #Pour la temperature
 
@@ -138,93 +166,90 @@ if [ "$temperature" -eq 1 ]
        temperature1=$temperature
        #awk  -F BEGIN{OFS=";"} FS=";" '{print $1,$2,$11,$12,$13}'  meteo_filtered_data_v1.csv > temp1.csv
        cut -d';' -f"$numero_station","$numero_date","$numero_temperature","$numero_temp_min","$numero_temp_max" "$fichierdentree" > temp1.csv
-       option_oblig+=1
-       #echo $option_oblig
-       echo "bien joué en cours de transfert"
+       
+       #echo "$option_oblig"
+       #echo "bien joué en cours de transfert"
 fi
 if [ "$temperature" -eq 2 ]
         then
         temperature2=$temperature
         cut -d';' -f"$numero_date","$numero_temperature" "$fichierdentree" > temp2.csv
         #echo ${temperature2}
-        echo "bien joué"
+        #echo "bien joué"
 fi
 if [ "$temperature" -eq 3 ]
         then
         temperature3=$temperature
-        cut -d';' -f"$numero_station" "$fichierdentree" > temp3.csv
+        cut -d';' -f"$numero_station","$numero_date","$numero_temperature" "$fichierdentree" > temp3.csv
         #echo "bien joué"
 fi        
 
-#Pour la pression
+# Condition pour la pression
 
 
 if [ "$pression" -eq 1 ]
        then
        temperature1=$temperature
        #awk  -F BEGIN{OFS=";"} FS=";" '{print $1,$2,$11,$12,$13}'  meteo_filtered_data_v1.csv > temp1.csv
-       cut -d';' -f1,2,7,8 $fichierdentree > press1.csv
-       option_oblig+=1
-       #echo $option_oblig
+       cut -d';' -f"$numero_station","$numero_pression_station","$numero_variation_pression" "$fichierdentree" > press1.csv
+       
+       #echo "$option_oblig"
        #echo "bien joué en cours de transfert"
 fi
 if [ "$pression" -eq 2 ]
        then
        temperature1=$temperature
        #awk  -F BEGIN{OFS=";"} FS=";" '{print $1,$2,$11,$12,$13}'  meteo_filtered_data_v1.csv > temp1.csv
-       cut -d';' -f2,7,8 $fichierdentree > press2.csv
+       cut -d';' -f"$numero_date","$numero_pression_station","$numero_variation_pression" "$fichierdentree" > press2.csv
        #echo "bien joué en cours de transfert"
 fi
 if [ "$pression" -eq 3 ]
        then
        temperature1=$temperature
        #awk  -F BEGIN{OFS=";"} FS=";" '{print $1,$2,$11,$12,$13}'  meteo_filtered_data_v1.csv > temp1.csv
-       cut -d';' -f1,2,7 $fichierdentree > press3.csv
+       cut -d';' -f"$numero_station","$numero_date","$numero_pression_station","$numero_variation_pression" "$fichierdentree" > press3.csv
        #echo "bien joué en cours de transfert"
 fi
 
+# Condition pour le vent
+
+if [ "$vent" == "w" ];then
+    echo "bien joué"
+    cut -d';' -f"$numero_station","$numero_direct_vent","$numero_vitesse_du_vent_moy" "$fichierdentree" > vent.csv
+fi
+
+# Condition pour l'altitude
+
+if [ "$altitude" == "h" ];then
+    echo "bien joué"
+    cut -d';' -f"$numero_station","$numero_altitude" "$fichierdentree" > altitude.csv
+fi
+
+# Condition pour l'humidité
+
+if [ "$humidite" == "m" ];then
+    echo "bien joué"
+   cut -d';' -f"$numero_station","$numero_humidite" "$fichierdentree" > humidite.csv
+fi
+
+
 #echo "$option_oblig"
 
-if (( $option_oblig < 1));then
+# Test pour les options obligatoire et geographiques
+
+if (( "$option_oblig" < 1));then
     echo "Impossible aucune option saisie"
     exit 1
 fi
 
+if (( "$option_geo" > 1));then
+    echo "Impossible d'executer trop d'option de lieux choisie."
+    exit 1
+fi
+
+
 shift $((OPTIND-1))
 echo "Analyse des options terminée"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
