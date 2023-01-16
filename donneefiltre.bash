63f98617092127d5828fabaa2b75b71e7ba65500
IDEFIX95 <104372704+IDEFIX95@users.noreply.github.com>
2023-01-16 11:48:24 +0100

    Update donneefiltre.bash

--- donneefiltre.bash
+++ donneefiltre.bash
@@ -12,6 +12,10 @@ vent=0
 humidite=0
 altitude=0
 count=0
+x_max=0
+x_min=0
+y_max=0
+y_min=0
 
 fichierdentree=''
 option_oblig=0
@@ -34,13 +38,23 @@ numero_temp_max=13
 numero_altitude=14
 numero_commune=15
 
+
+# les variables pour les zones geographiques
+france_corse=0
+guyane_francaise=0
+saint_pierre_et_mique=0
+antille=0
+ocean_indien=0
+antarctique=0
+
+
 #if (( $# != 1));then
     #echo "Impossible aucune option saisie"
    # exit 1
 #fi
 
 ##Gestion des parametres
-while getopts ":whmf:d:t:p:-:" option;
+while getopts ":FGSAOQwhmf:d:t:p:-:" option;
 do
     # Commencons par la gestion des options longues
     if [ "$option" = '-' ]
@@ -122,23 +136,29 @@ do
             option_oblig=$(($option_oblig+1))
             ;;
         F)
+            france_corse=${option}
             #awk -F";" 'NR {print '$numero_coordonne'}' meteo_filtered_data_v1.csv
             option_geo=$(($option_geo+1))
             ;;
         G)
+            guyane_francaise=${option}
             option_geo=$(($option_geo+1))
         ;;
         S)
-            #awk -F ";" '{split('$numero_coordonne', coord, ";"); x = coord[1]; y = coord[2]; if (x < 48 && x > 46 && y > 55 && y < 57 ) {print x,y}}' meteo_filtered_data_v1.csv
+            saint_pierre_et_mique=${option}
+           # echo "bien joué"
             option_geo=$(($option_geo+1))
         ;;
         A)
+            antille=${option}
             option_geo=$(($option_geo+1))
         ;;
         O)
+            ocean_indien=${option}
             option_geo=$(($option_geo+1))
         ;;
         Q)
+            antarctique=${option}
             option_geo=$(($option_geo+1))
             echo "bonjour"
         ;;
@@ -163,7 +183,7 @@ done
 
 if [ "$temperature" -eq 1 ]
        then
-       temperature1=$temperature
+       #temperature1=$temperature
        #awk  -F BEGIN{OFS=";"} FS=";" '{print $1,$2,$11,$12,$13}'  meteo_filtered_data_v1.csv > temp1.csv
        cut -d';' -f"$numero_station","$numero_date","$numero_temperature","$numero_temp_min","$numero_temp_max" "$fichierdentree" > temp1.csv
        
@@ -172,14 +192,14 @@ if [ "$temperature" -eq 1 ]
 fi
 if [ "$temperature" -eq 2 ]
         then
-        temperature2=$temperature
+        #temperature2=$temperature
         cut -d';' -f"$numero_date","$numero_temperature" "$fichierdentree" > temp2.csv
         #echo ${temperature2}
         #echo "bien joué"
 fi
 if [ "$temperature" -eq 3 ]
         then
-        temperature3=$temperature
+        #temperature3=$temperature
         cut -d';' -f"$numero_station","$numero_date","$numero_temperature" "$fichierdentree" > temp3.csv
         #echo "bien joué"
 fi        
@@ -189,7 +209,7 @@ fi
 
 if [ "$pression" -eq 1 ]
        then
-       temperature1=$temperature
+       
        #awk  -F BEGIN{OFS=";"} FS=";" '{print $1,$2,$11,$12,$13}'  meteo_filtered_data_v1.csv > temp1.csv
        cut -d';' -f"$numero_station","$numero_pression_station","$numero_variation_pression" "$fichierdentree" > press1.csv
        
@@ -198,14 +218,13 @@ if [ "$pression" -eq 1 ]
 fi
 if [ "$pression" -eq 2 ]
        then
-       temperature1=$temperature
+       
        #awk  -F BEGIN{OFS=";"} FS=";" '{print $1,$2,$11,$12,$13}'  meteo_filtered_data_v1.csv > temp1.csv
        cut -d';' -f"$numero_date","$numero_pression_station","$numero_variation_pression" "$fichierdentree" > press2.csv
        #echo "bien joué en cours de transfert"
 fi
 if [ "$pression" -eq 3 ]
        then
-       temperature1=$temperature
        #awk  -F BEGIN{OFS=";"} FS=";" '{print $1,$2,$11,$12,$13}'  meteo_filtered_data_v1.csv > temp1.csv
        cut -d';' -f"$numero_station","$numero_date","$numero_pression_station","$numero_variation_pression" "$fichierdentree" > press3.csv
        #echo "bien joué en cours de transfert"
@@ -221,7 +240,7 @@ fi
 # Condition pour l'altitude
 
 if [ "$altitude" == "h" ];then
-    echo "bien joué"
+   # echo "bien joué"
     cut -d';' -f"$numero_station","$numero_altitude" "$fichierdentree" > altitude.csv
 fi
 
@@ -233,14 +252,52 @@ if [ "$humidite" == "m" ];then
 fi
 
 
+## Condition pour les lieux geographiques
+
+if [ "$france_corse" == "F" ];then
+    echo "bien joué"
+     awk -F ";" '{split('$numero_coordonne', coord, ";"); x = coord[1]; y = coord[2]; if (x < 48 && x > 46 && y > 55 && y < 57 ) {print x,y}}' "$fichierdentree"
+fi  
+
+
+if [ "$guyane_francaise" == "G" ];then
+    echo "bien joué"
+    awk -F ";" '{split('$numero_coordonne', coord, ";"); x = coord[1]; y = coord[2]; if (x < 48 && x > 46 && y > 55 && y < 57 ) {print x,y}}' "$fichierdentree"
+fi  
+
+
+if [ "$saint_pierre_et_mique" == "S" ];then
+    echo "bien joué"
+    
+    grep $x_max=48 $x_min=46 $y_min=55 $y_max=57 | awk -F ";" '{split( $numero_coordonne, coord, ";"); x = coord[1]; y = coord[2]; if (x < 48 && x > 46 && y > 55 && y < 57 ) {print x,y}}' "$fichierdentree" 
+fi  
+
+
+if [ "$antille" == "A" ];then
+    echo "bien joué"
+    awk -F ";" '{split( $numero_coordonne, coord, ";"); x = coord[1]; y = coord[2]; if (x < 48 && x > 46 && y > 55 && y < 57 ) {print x,y}}' "$fichierdentree"
+fi  
+
+
+if [ "$ocean_indien" == "O" ];then
+    echo "bien joué"
+     awk -F ";" '{split('$numero_coordonne', coord, ";"); x = coord[1]; y = coord[2]; if (x < 48 && x > 46 && y > 55 && y < 57 ) {print x,y}}' "$fichierdentree"
+fi  
+
+
+if [ "$antarctique" == "Q" ];then
+    echo "bien joué"
+     awk -F ";" '{split('$numero_coordonne', coord, ";"); x = coord[1]; y = coord[2]; if (x < 48 && x > 46 && y > 55 && y < 57 ) {print x,y}}' "$fichierdentree"
+fi  
+
 #echo "$option_oblig"
 
 # Test pour les options obligatoire et geographiques
 
-if (( "$option_oblig" < 1));then
-    echo "Impossible aucune option saisie"
-    exit 1
-fi
+#if (( "$option_oblig" < 1));then
+#    echo "Impossible aucune option saisie"
+ #   exit 1
+#fi
 
 if (( "$option_geo" > 1));then
     echo "Impossible d'executer trop d'option de lieux choisie."
@@ -250,13 +307,6 @@ fi
 
 shift $((OPTIND-1))
 echo "Analyse des options terminée"
-
-
-
-
-
-
-
 
 
 
